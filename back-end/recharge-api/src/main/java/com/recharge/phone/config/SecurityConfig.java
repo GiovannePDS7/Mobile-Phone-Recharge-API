1d42a15f2d856ee0833cbe32baac3dc1d9b48d89
Giovanne Pagano dos Santos <giovanne3282@gmail.com>
2026-07-28 23:11:27 -0300

    add: swagger endpoint - contract first

--- back-end/recharge-api/src/main/java/com/recharge/phone/config/SecurityConfig.java
+++ back-end/recharge-api/src/main/java/com/recharge/phone/config/SecurityConfig.java
@@ -17,7 +17,8 @@ public class SecurityConfig {
                 .requestMatchers(
                     "/swagger-ui/**",
                     "/v3/api-docs/**",
-                    "/swagger-ui.html"
+                    "/swagger-ui.html",
+                    "/openapi/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             );
